a3225e577ea364ea775357f5b0419903431f0caa
Stephanie Reed <sreed19@my.bcit.ca>
2017-05-10 12:22:10 -0700

    (Placeholder) side-scrolling implemented
    NOTE: Canvas width and height need to be set at a later date.

--- js/gameplay.js
+++ js/gameplay.js
@@ -157,18 +157,135 @@ function resetAnswerButtons() {
 function addRandomItem() {
     // IGNORE
 }
-$("#divLevelArt").click(function(){
+$("#divLevelArea").click(function(){
     addItem("PLACEHOLDER");
 });
 
-
 // UI Debug Script 
 /*
-$("#divLevelArt").click(function(){
+$("#divLevelArea").click(function(){
     swapButtons();
     toggleDialogue(); 
 });
 */
+
+// Side-Scrolling
+var playerChar;
+var myBackground;
+
+function startGame() {
+    playerChar = new component(80, 80, "images/placeholder/char.gif", 30, 190, "image");
+    background = new component(800, 310, "images/placeholder/1.png", 0, 0, "background");
+	
+    gameArea.start();
+    
+}
+
+
+var gameArea = {
+    canvas : $("#divLevelArea").children("canvas")[0],
+    start : function() {
+        alert(this.canvas); 
+        this.canvas = $("#divLevelArea").children("canvas")[0];
+        this.canvas.width = 480; 
+        this.canvas.height = 300; 
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 10);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+/*
+function gameArea() {
+    this.canvas = $("#divLevelArea").children("canvas")[0];
+    this.context = this.canvas.getContext("2d");
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 10);
+    
+}
+*/
+
+/*
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 300;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 10);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+*/
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image" || type == "background") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = gameArea.context;
+        if (type == "image" || type == "background") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "background") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.type == "background") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    }    
+}
+
+function updateGameArea() {
+    gameArea.clear();
+    background.speedX = -1;
+    background.newPos();    
+    background.update();
+    playerChar.newPos();    
+    playerChar.update();
+}
+
+
+$("#divLevelArea").ready(startGame);
+
+
+
 
 // Combat Script
 var blockInput = false; 

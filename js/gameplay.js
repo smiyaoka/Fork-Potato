a966dded7ce65410a7b8b3972a8558c680269dd6
Stephanie Reed <sreed19@my.bcit.ca>
2017-05-11 21:52:26 -0700

    Partial commenting and code-reorganizing (still WIP)

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,7 +1,9 @@
 //connect to firebase.
  var ref = new Firebase("https://project-fork-and-potato.firebaseio.com/questions");
 
-// UI Functions
+// UI FUNCTIONS 
+
+// Switches between the combat buttons and the trivia buttons. 
 function swapButtons() {
     if ($("#divCombatButtons").css("display") == "flex") {
         $("#divCombatButtons").css("display", "none");
@@ -12,6 +14,7 @@ function swapButtons() {
     }                
 }
 
+// Hides and unhides the question box at the top of the screen. 
 function toggleQuestion() {
     /*
     if ($("#divDialogue").css("display") == "block") {
@@ -25,6 +28,7 @@ function toggleQuestion() {
     }
 }
 
+// Hides and unhides the dialogue box at the top of the screen. 
 function toggleDialogue() {
     /*
     if ($("#divQuestion").css("display") == "block") {
@@ -38,12 +42,63 @@ function toggleDialogue() {
     }
 }
 
-// Combat Functions
+// GAME FLOW FUNCTIONS 
+
+// Called when the level is successfully completed. 
+// This function is currently a placeholder. 
+function levelComplete() {
+    blockInput = true; 
+    console.log("LEVEL COMPLETE"); 
+    toggleDialogue();
+    $("#divDialogue").html("Level Complete");
+}
+
+// Called when the player fails the level. 
+// This function is currently a placeholder. 
+function gameOver() {
+    blockInput = true; 
+    freeze = true; 
+    console.log("GAME OVER"); 
+    toggleDialogue();
+    $("#divDialogue").html("Game Over");
+}
+
+// COMBAT FUNCTIONS
+
+// Deals damage to a specific enemy. 
+// @param target The enemy. 
+// @param damage The damage to be dealt. 
+function hurtEnemy(target, damage) {
+    target.hp -= damage; 
+}
+
+// Deals damage to the player. 
+// @param damage The damage to be dealt. 
+function hurtPlayer(damage) {
+    playerChar.hp -= damage; 
+    refresh();
+    if (playerChar.hp <= 0) {
+        gameOver();
+    }
+}
+
+// The range of the user's eat attack, specifically, the maximum 
+// distance between the player's right side and the enemy's 
+// left side. 
+var eatRange = 80;  
+
+// The amount of damage done by the eat attack. 
+var eatDamage = 1; 
+
+// Use the eat attack, which deals damage to the closest enemy 
+// within a short range. 
+// This is called when the user clicks the eat button. 
 function clickEat() {
     if (blockInput) 
-        return;     
-    var eatRange = 80; 
-    var farEnd = eatRange + playerChar.x + playerChar.width; 
+        return;    
+    // Calculate the range of the attack. 
+    var farEnd = eatRange + playerChar.x + playerChar.width;     
+    // Determine the closest enemy within range 
     var target = null;
     enemies.forEach(function(part, index, arr){
         if (arr[index].x < farEnd) {
@@ -53,65 +108,102 @@ function clickEat() {
                 target = arr[index];
             }
         }
-    });
+    });    
+    // If a target is found, deal damage. 
     if (target != null) {
-        var eatDamage = 1; 
         hurtEnemy(target, eatDamage);
     }
 }
 
-var skillCooldown = false; 
-var skillCooldownTime = 5000; 
+// The range of the user's skill attack, specifically, the maximum 
+// distance between the player's right side and the enemy's 
+// left side. 
+var skillRange = 350; 
+
+// The amount of damage done by the skill attack. 
+var skillDamage = 3; 
+
+// This is true if the skill attack is currently on cooldown. 
+var skillOnCooldown = false; 
+
+// The skill's cooldown interval, measured in milliseconds. 
+var skillCooldownInterval = 5000; 
+
+//The skill's timer. 
 var skillTimer; 
+
+// The time on the current skill cooldown timer, 
+//measured in milliseconds. 
+var skillCooldownTime; 
+
+// Use the Skill attack, which deals damage to all enemies 
+// within a long range. 
+// This is called when the user clicks the skill button. 
 function clickSkill() {
     if (blockInput) 
         return;
-    if (skillCooldown) 
+    if (skillOnCooldown) 
         return; 
-    
-    var skillDamage = 3; 
-    var skillRange = 350; 
+    // Calculate the skill's range. 
     var farEnd = skillRange + playerChar.x + playerChar.width; 
+    // Damage all enemies within range. 
     enemies.forEach(function(part, index, arr){
         if (arr[index].x < farEnd) {
             hurtEnemy(arr[index], skillDamage); 
         }
     });    
-    skillCooldown = true;     
-    hurtEnemy(skillDamage);
-    skillCooldownTime = 5000; 
+    // Start the skill's cooldown. 
+    skillOnCooldown = true;     
+    skillCooldownTime = skillCooldownInterval; 
     skillTimer = setInterval(skillUpdate, 100);     
 }
 
+// The function used along with setInterval to implement the 
+// skill attack's cooldown. 
 function skillUpdate() {
     if (freeze) 
         return; 
-    skillCooldownTime -= 100; 
-    console.log(skillCooldownTime/1000); 
+    // Decrease the cooldown time. 
+    skillCooldownTime -= 100;     
     if(skillCooldownTime <= 0) {
-        $("#divCombatButton2").html("Skill<br>Does 3 damage to range.");
-        skillCooldown = false; 
+        /// If time's up, turn off cooldown. 
+        $("#divCombatButton2").html(
+            "Skill<br>Does 3 damage to range.");
+        skillOnCooldown = false; 
         clearInterval(skillTimer); 
     } else {
-        $("#divCombatButton2").html(skillCooldownTime/1000);
+        // Otherwise, display the cooldown time in the button. 
+        $("#divCombatButton2").html(skillCooldownInterval/1000);
     }
 }
 
+// The amount of hp healed when the player uses an item. 
+var itemHealing = 3; 
+
+// Use an item. This is called when the user clicks an item button. 
+// Currently, all items just heal the player's health. 
+// @param number The number of the combat button. 
 function clickItem(number) {
     if(blockInput) 
         return; 
+    // Check if the user has an item in that slot. 
     if(!items[number])
         return; 
-    playerChar.hp += 3; 
+    // Heal the player. 
+    playerChar.hp += itemHealing; 
     if (playerChar.hp > playerMaxHP) 
         playerChar.hp = playerMaxHP; 
-    //hurtEnemy(itemDamage[items[number]]); 
+    // Remove the item from that slot. 
     items[number] = null; 
     $("#divCombatButton" + number).empty();
 }
 
+// Adds an item to the nearest empty item slot. 
+// If there is no room, no item is added. 
+// @param The name of the item. 
 function addItem(item) {
     var searching = true; 
+   // Item slots are combat buttons 3-5. 
     for (var i = 3; i <= 5 && searching; i++) {
         if (!items[i]) {
             items[i] = item; 
@@ -121,17 +213,9 @@ function addItem(item) {
     }
 }
 
-function hurtEnemy(target, damage) {
-    target.hp -= damage; 
-}
 
-function hurtPlayer(damage) {
-    playerChar.hp -= damage; 
-    refresh();
-    if (playerChar.hp <= 0) {
-        gameOver();
-    }
-}
+// TRIVIA FUNCTIONS 
+
 
 function startTrivia() {
     swapButtons();
@@ -144,22 +228,8 @@ function startTrivia() {
     nextQuestion();
 }
 
-function levelComplete() {
-    blockInput = true; 
-    console.log("LEVEL COMPLETE"); // DEBUG
-    toggleDialogue();
-    $("#divDialogue").html("Level Complete");
-}
 
-function gameOver() {
-    blockInput = true; 
-    freeze = true; 
-    console.log("GAME OVER"); // DEBUG
-    toggleDialogue();
-    $("#divDialogue").html("Game Over");
-}
 
-// Trivia Functions 
 function clickAnswer(number) {    
     if (blockInput) 
         return;     
@@ -361,14 +431,13 @@ function component(width, height, color, x, y, type, initialHP, speedX) {
 function skillUpdate() {
     if (freeze) 
         return; 
-    skillCooldownTime -= 100; 
-    console.log(skillCooldownTime/1000); 
-    if(skillCooldownTime <= 0) {
+    skillCooldownInterval -= 100; 
+    if(skillCooldownInterval <= 0) {
         $("#divCombatButton2").html("Skill<br>Does 3 damage to range.");
-        skillCooldown = false; 
+        skillOnCooldown = false; 
         clearInterval(skillTimer); 
     } else {
-        $("#divCombatButton2").html(skillCooldownTime/1000);
+        $("#divCombatButton2").html(skillCooldownInterval/1000);
     }
 }
 
